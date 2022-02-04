07bd8c39c539a9597f76d15e84e7a2274c9bf6f5
CodeIGuess <codeiguess@gmail.com>
2022-02-04 13:03:28 -0400

    Started work on PhysicsActor, with accessibility modifiers

--- Drops/PhysicsActor.ts
+++ Drops/PhysicsActor.ts
@@ -1,4 +1,18 @@
 
+type PhysicsProperties = {physics: number, gravity: Vec2, friction: Vec2, groundFriction: Vec2}
+
 class PhysicsActor extends Sprite {
-	
+	public static PHYSICS_LOOP = 1 // Friction, gravity and inertia, no collision
+	public static PHYSICS_HARD = 2 // Hard collisions with other physics objects
+	public static PHYSICS_SOFT = 4 // Soft collisions that push objects away slowly
+
+	public physics: PhysicsProperties
+	public constructor(src: Img, x: number, y: number, width = -1, height = -1) {
+		super(src, x, y, width, height)
+	}
+
+	public intersects(sprite: Sprite, colliderNum = -1) {
+		const b1 = this.getHb()
+		const b2 = sprite.getHb()
+	}
 }
