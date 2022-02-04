88b94fd1d133057192dde22b9d7dd9393deafdc5
CodeIGuess <codeiguess@gmail.com>
2022-02-03 23:12:17 -0400

    Quit toying with image generation, started going for physics!

--- TestGame/main.ts
+++ TestGame/main.ts
@@ -1,34 +1,15 @@
 
 const g: Glass = new Glass(200, [100, 100, 100])
 
-const d = 2
+const d = 3
 
 const spr = g.scene.nObj(new Sprite(
-	new ImgGen(128, 128)
-		.c(50, 100, 255)
-		.noise(d)
-
-		.c(255, 255, 0)
-		.noise(d, 120)
-		
-		.c(20, 255, 80)
-		.noise(d, 135)
-
-		.c(170, 255, 190)
-		.noise(d, 170)
-
-		.c(220, 220, 220)
-		.noise(d, 180)
-
-		.done()
-	// new ImgURL("test.png")
+	new ImgURL("test.png")
 	, 0, 0))
 
-g.init(() => {
-	Log.p("Did init.")
-})
+g.init(() => {  })
 
 g.frame(() => {
-	// spr.pos.x = (Math.sin(Surface.frameCount / 70) + 1) * 0.5 * (Surface.texture.width - spr.width)
-	// Log.p("Frame!", Surface.frameCount)
+	spr.pos.x = (Math.sin(Surface.frameCount / 73) + 1) * 0.5 * (Surface.texture.width - spr.width)
+	spr.pos.y = (Math.sin(Surface.frameCount / 41) + 1) * 0.5 * (Surface.texture.height - spr.height)
 })
