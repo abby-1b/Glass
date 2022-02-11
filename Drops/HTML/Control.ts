f686e1451fe81c6e70d5f181d4f764ca14afc8d4
CodeIGuess <codeiguess@gmail.com>
2022-02-10 23:58:20 -0400

    Made Controller static!

--- Drops/HTML/Control.ts
+++ Drops/HTML/Control.ts
@@ -1,29 +1,39 @@
 class Control {
-	public touch: boolean = ("ontouchend" in document)
-	public keyboard = true //!this.touch
+	public static touch: boolean = ("ontouchend" in document)
+	public static keyboard = true //!this.touch
 
-	private cEvents: { [key: string]: [CallbackFunction, boolean] } = {}
+	public static mouseX = 0
+	public static mouseY = 0
 
-	public cEvent(name: string, fn: CallbackFunction): void {
+	private static cEvents: { [key: string]: [CallbackFunction, boolean] } = {}
+
+	static {
+		window.addEventListener("mousemove", (e) => {
+			this.mouseX = (e.clientX / window.innerWidth) * Surface.width
+			this.mouseY = (e.clientY / window.innerHeight) * Surface.height
+		})
+	}
+
+	public static cEvent(name: string, fn: CallbackFunction): void {
 		this.cEvents[name] = [fn, false]
 	}
 
 	// Keyboard
-	public onKeyDown(keys: Array<string>, cEv: string): void {
-		if (!(cEv in this.cEvents)) Log.w("Event", cEv, "doesn't exist.")
+	public static onKeyDown(keys: Array<string>, eventName: string): void {
+		if (!(eventName in this.cEvents)) Log.w("Event", eventName, "doesn't exist.")
 		window.addEventListener("keydown", e => {
 			if (keys.includes(e.key) && !e.repeat) {
-				this.cEvents[cEv][0]()
-				this.cEvents[cEv][1] = true
+				this.cEvents[eventName][0]()
+				this.cEvents[eventName][1] = true
 			}
 		})
 		window.addEventListener("keyup", e => {
 			if (keys.includes(e.key))
-				this.cEvents[cEv][1] = false
+				this.cEvents[eventName][1] = false
 		})
 	}
 
-	public isOngoing(cEv: string): boolean {
+	public static isOngoing(cEv: string): boolean {
 		return this.cEvents[cEv][1]
 	}
 }
