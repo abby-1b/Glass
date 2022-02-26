61245b13d89028efb75c28628c4c8dd61b9f72f6
CodeIGuess <codeiguess@gmail.com>
2022-02-26 16:58:54 -0400

    Reindented, widened use from `Img` to`TextureCanvas`

--- Drops/Sprite.ts
+++ Drops/Sprite.ts
@@ -1,8 +1,11 @@
 
 type HitboxOffsets = {top: number, bottom: number, left: number, right: number}
 
+/**
+ * Any object that can be drawn to the screen.
+ */
 class Sprite {
-	public src: Img
+	public src: TextureCanvas
 	public parent: Scene
 
 	public pos: Vec2 = new Vec2(0, 0)
@@ -15,9 +18,10 @@ class Sprite {
 
 	public hbOffsets = {top: 0, bottom: 0, left: 0, right: 0}
 
+	public showHb = true
 	private hb: Rect = new Rect(0, 0, 0, 0, true)
 
-	public constructor(src: Img, x: number, y: number, width = -1, height = -1) {
+	public constructor(src: TextureCanvas, x: number, y: number, width = -1, height = -1) {
 		this.src = src
 		this.pos.x = x
 		this.pos.y = y
@@ -37,21 +41,22 @@ class Sprite {
 	}
 
 	public draw(): void {
+		// TODO: draw centered
 		Surface.texture.drawImage(this.src, this.pos, this.width, this.height, 1, this.flipped)
-		this.drawHb()
+		if (this.showHb) this.drawHb()
 	}
 
 	public getHb(): Rect {
 		if (this.flipped) {
-			this.hb.x      = this.pos.x - (this.centered ? this.width * 0.5 : 0) + this.hbOffsets.right * this.scale,
-			this.hb.y      = this.pos.y - (this.centered ? this.height * 0.5 : 0) + this.hbOffsets.top * this.scale,
-			this.hb.width  = this.width * this.scale - (this.hbOffsets.right * this.scale + this.hbOffsets.left * this.scale),
-			this.hb.height = this.height * this.scale - (this.hbOffsets.top * this.scale + this.hbOffsets.bottom * this.scale)
+			this.hb.x		= this.pos.x - (this.centered ? this.width * 0.5 : 0) + this.hbOffsets.right * this.scale,
+			this.hb.y		= this.pos.y - (this.centered ? this.height * 0.5 : 0) + this.hbOffsets.top * this.scale,
+			this.hb.width	= this.width * this.scale - (this.hbOffsets.right * this.scale + this.hbOffsets.left * this.scale),
+			this.hb.height	= this.height * this.scale - (this.hbOffsets.top * this.scale + this.hbOffsets.bottom * this.scale)
 		} else {
-			this.hb.x      = this.pos.x - (this.centered ? this.width * 0.5 : 0) + this.hbOffsets.left * this.scale,
-			this.hb.y      = this.pos.y - (this.centered ? this.height * 0.5 : 0) + this.hbOffsets.top * this.scale,
-			this.hb.width  = this.width * this.scale - (this.hbOffsets.left * this.scale + this.hbOffsets.right * this.scale),
-			this.hb.height = this.height * this.scale - (this.hbOffsets.top * this.scale + this.hbOffsets.bottom * this.scale)
+			this.hb.x		= this.pos.x - (this.centered ? this.width * 0.5 : 0) + this.hbOffsets.left * this.scale,
+			this.hb.y		= this.pos.y - (this.centered ? this.height * 0.5 : 0) + this.hbOffsets.top * this.scale,
+			this.hb.width	= this.width * this.scale - (this.hbOffsets.left * this.scale + this.hbOffsets.right * this.scale),
+			this.hb.height	= this.height * this.scale - (this.hbOffsets.top * this.scale + this.hbOffsets.bottom * this.scale)
 		}
 		this.hb.reload()
 		return this.hb
