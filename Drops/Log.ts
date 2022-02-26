c564809b2b3c223d07d590b3642fa333797b0d3f
CodeIGuess <codeiguess@gmail.com>
2022-02-26 16:58:24 -0400

    Can now print any message, including a class tree

--- Drops/Log.ts
+++ Drops/Log.ts
@@ -1,6 +1,7 @@
 class Log {
-	public static p(msg: string): void {
-		console.log(msg)
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public static p(...args: any): void {
+		console.log(...args)
 	}
 
 	public static e(...things: Array<string>): void {
@@ -11,5 +12,16 @@ class Log {
 
 	public static w(...things: Array<string>): void {
 		console.warn(...things)
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public static classTree(c: any): void {
+		const prt: string[] = []
+		if (c.name) prt.push(c.name)
+		while (c.__proto__.name != "") {
+			prt.unshift(c.__proto__.name)
+			c = c.__proto__
+		}
+		console.log(prt.map((e, i) => (i == 0 ? "  " : "   ".repeat(i)) + (i == 0 ? "" : String.fromCharCode(9492) + String.fromCharCode(9588)) + e).join("\n"))
 	}
 }
