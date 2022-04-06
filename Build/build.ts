04ee607a58b26f3e318adcc8c3fcf8e15c25f959
CodeIGuess <codeiguess@gmail.com>
2022-04-05 22:52:19 -0400

    Made exporting actually work

--- Build/build.ts
+++ Build/build.ts
@@ -5,29 +5,45 @@ import { Language, minify } from "https://deno.land/x/minifier/mod.ts"
 const FULL = Deno.args.includes("full")
 
 // Gets the file name to export to
-const FILE = Deno.args.filter(e => e.endsWith(".js"))[0] ?? "out.js"
+const FILE = Deno.args.filter(e => e.endsWith(".ts"))[0]
+
+if (!FILE) {
+	console.log("No file provided!")
+	Deno.exit()
+}
 
 // Get the TypeScript sources for the drops
 const dropOrder: string[] = JSON.parse(Deno.readTextFileSync("../dropOrder.json"))
 	.map((e: string) => "../Drops/" + e + ".ts")
+
+dropOrder.push(FILE)
+
 const dropCode = dropOrder.map((e: string) => {
-	return {[e.replace("../Drops", "")]: Deno.readTextFileSync(e)}
+	return {[e.replace("../Drops", "").replace("../", "/")]: Deno.readTextFileSync(e)}
 })
 
 // Compile sources
 const compiled = await Deno.emit("/mod.ts", {
 	sources: Object.assign({
-		"/mod.ts": dropOrder.map(e => `import * as ${e.replace("../Drops/", "").replace("/", "_").replace(".ts", "")} from "${e.replace("../Drops", "")}"`).join(";")
+		"/mod.ts": dropOrder.map(e => `import * as ${e.replace(/\.\.\//g, "").replace(/\//g, "_").replace(".ts", "")} from "${e.replace("../Drops", "")}"`).join(";\n")
 	}, ...dropCode), compilerOptions: { lib: [ "esnext", "dom", "dom.iterable" ] }
 })
 
+console.log(dropOrder.map(e => `import * as ${e.replace(/\.\.\//g, "").replace(/\//g, "_").replace(".ts", "")} from "${e.replace("../Drops", "")}"`).join(";\n"))
+console.log(compiled)
+
 // Join files into one string
-let built = Object.values(compiled.files)
-	.filter(f => f[0] == "\"")
+const sorted: string[] = []
+for (const f in dropOrder) {
+	// console.log(dropOrder[f])
+	sorted.push(compiled.files["file://" + dropOrder[f].replace("../Drops", "").replace("../", "/") + ".js"])
+}
+
+let built = sorted
 	.map(e => e.replace("\"use strict\";\n", ""))
 	.join("\n")
 
 if (FULL)
 	built = minify(Language.JS, built)
 
-Deno.writeTextFileSync(FILE, built)
+Deno.writeTextFileSync("out.js", built)
