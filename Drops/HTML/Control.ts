b5219e61498b8d411287f039a750f6fef157f6a1
CodeIGuess <codeiguess@gmail.com>
2022-04-06 22:29:03 -0400

    Added `mouseDown` property!

--- Drops/HTML/Control.ts
+++ Drops/HTML/Control.ts
@@ -1,9 +1,11 @@
+
 class Control {
 	public static touch: boolean = ("ontouchend" in document)
 	public static keyboard = true //!this.touch
 
 	public static mouseX = 0
 	public static mouseY = 0
+	public static mouseDown = false
 
 	public static currentTouches: {[key: number]: number} = {}
 
@@ -35,6 +37,8 @@ class Control {
 	public static touchArea(columns: number, rows: number, eventNames: string[]): void {
 		// if (!(eventName in this.cEvents)) Log.w("Event", eventName, "doesn't exist.")
 		window.addEventListener("touchstart", function(e) {
+			e.preventDefault()
+			document.body.style.overflow = "hidden"
 			for (let ct = 0; ct < e.changedTouches.length; ct++) {
 				const i = Math.floor(Math.round(e.changedTouches[ct].clientX) * columns / (window.innerWidth + 1))
 					+ columns * Math.floor(Math.round(e.changedTouches[ct].clientY) * rows / (window.innerHeight + 1))
@@ -48,6 +52,7 @@ class Control {
 			}
 		})
 		window.addEventListener("touchmove", function(e) {
+			e.preventDefault()
 			for (let ct = 0; ct < e.changedTouches.length; ct++) {
 				const i = Math.floor(Math.round(e.changedTouches[ct].clientX) * columns / (window.innerWidth  + 1))
 					+ columns * Math.floor(Math.round(e.changedTouches[ct].clientY) * rows / (window.innerHeight + 1))
@@ -60,6 +65,7 @@ class Control {
 			}
 		})
 		window.addEventListener("touchend", function(e) {
+			e.preventDefault()
 			for (let ct = 0; ct < e.changedTouches.length; ct++) {
 				Control.cEvents[eventNames[Control.currentTouches[e.changedTouches[ct].identifier]]][1] = false
 				delete Control.currentTouches[e.changedTouches[ct].identifier]
@@ -73,3 +79,6 @@ window.addEventListener("mousemove", (e) => {
 	Control.mouseX = (e.clientX / window.innerWidth) * Surface.width
 	Control.mouseY = (e.clientY / window.innerHeight) * Surface.height
 })
+
+window.addEventListener("mousedown", () => { Control.mouseDown = true })
+window.addEventListener("mouseup", () => { Control.mouseDown = false })
