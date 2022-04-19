70700255f32ef2a1cfa5b59980a8dbe70acd7c5d
CodeIGuess <codeiguess@gmail.com>
2022-04-18 20:34:19 -0400

    Deleted commented code

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,6 @@ module.exports =
 	"rules": {
 		"@typescript-eslint/no-empty-function": [ "off" ],
 		"@typescript-eslint/no-unused-vars": [ "off" ],
-		"@typescript-eslint/explicit-member-accessibility": [ "error" ],
 		"@typescript-eslint/explicit-function-return-type": [ "error" ],
 		"indent": [
 			"warn",
@@ -40,49 +39,3 @@ module.exports =
 		]
 	}
 }
-
-// {
-// 	"root": true,
-// 	"parser": "@typescript-eslint/parser",
-// 	"parserOptions": {
-// 	  "createDefaultProgram": true
-// 	},
-// 	"plugins": ["@typescript-eslint"],
-// 	"extends": [
-// 	  "plugin:@typescript-eslint/recommended",
-// 	],
-// 	"rules": {
-// 		"no-unused-vars": "off",
-// 		"@typescript-eslint/no-unused-vars": [ "warning" ],
-// 		// "@typescript-eslint/no-unused-vars": [
-// 		// 	"error",
-// 		// 	{ "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }
-// 		// ],
-// 		"@typescript-eslint/array-type": [ "error", { "default": "array-simple" } ],
-// 		"@typescript-eslint/ban-ts-comment": ["off"],
-// 		"@typescript-eslint/explicit-member-accessibility": [ "error" ],
-// 		"@typescript-eslint/explicit-module-boundary-types": [ "off" ],
-// 		"@typescript-eslint/no-non-null-assertion": [ "off" ],
-// 		"@typescript-eslint/no-use-before-define": [ "off" ],
-// 		"@typescript-eslint/no-parameter-properties": [ "off" ],
-// 		"@typescript-eslint/ban-ts-ignore": [ "off" ],
-// 		"@typescript-eslint/no-empty-function": [ "off" ],
-// 		"no-return-await": "error",
-// 		"require-await": "error",
-// 		"no-async-promise-executor": "error"
-// 	},
-// 	// "overrides": [
-// 	//   {
-// 	// 	"files": [ "*.js" ],
-// 	// 	"rules": {
-// 	// 	  "@typescript-eslint/explicit-function-return-type": [ "off" ]
-// 	// 	}
-// 	//   },
-// 	//   {
-// 	// 	"files": ["tools/node_*.js"],
-// 	// 	"rules": {
-// 	// 	  "@typescript-eslint/no-var-requires": [ "off" ]
-// 	// 	}
-// 	//   }
-// 	// ]
-// }
