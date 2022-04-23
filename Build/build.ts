66fbc5247444b0cfa1e9a12ccefa7f68786c93e9
CodeIGuess <codeiguess@gmail.com>
2022-04-22 22:36:19 -0400

    Fully switched over to TypeScript for builds

--- Build/build.ts
+++ Build/build.ts
@@ -1,49 +1,22 @@
 
-import { Language, minify } from "https://deno.land/x/minifier/mod.ts"
-
-// Specifies wether or not to fully minify the file
-const FULL = Deno.args.includes("full")
-
-// Gets the file name to export to
-const FILE = Deno.args.filter(e => e.endsWith(".ts"))[0]
-
-if (!FILE) {
-	console.log("No file provided!")
-	Deno.exit()
+async function buildTS(ts: string): Promise<string> {
+	return (await Deno.emit("/mod.ts", {
+		sources: { "/mod.ts": ts },
+	})).files["file:///mod.ts.js"]
 }
 
-// Get the TypeScript sources for the drops
-const dropOrder: string[] = JSON.parse(Deno.readTextFileSync("../dropOrder.json"))
-	.map((e: string) => "../Drops/" + e + ".ts")
-
-dropOrder.push(FILE)
-
-const dropCode = dropOrder.map((e: string) => {
-	return {[e.replace("../Drops", "").replace("../", "/")]: Deno.readTextFileSync(e)}
-})
-
-// Compile sources
-const compiled = await Deno.emit("/mod.ts", {
-	sources: Object.assign({
-		"/mod.ts": dropOrder.map(e => `import * as ${e.replace(/\.\.\//g, "").replace(/\//g, "_").replace(".ts", "")} from "${e.replace("../Drops", "")}"`).join(";\n")
-	}, ...dropCode), compilerOptions: { lib: [ "esnext", "dom", "dom.iterable" ] }
-})
-
-console.log(dropOrder.map(e => `import * as ${e.replace(/\.\.\//g, "").replace(/\//g, "_").replace(".ts", "")} from "${e.replace("../Drops", "")}"`).join(";\n"))
-console.log(compiled)
-
-// Join files into one string
-const sorted: string[] = []
-for (const f in dropOrder) {
-	// console.log(dropOrder[f])
-	sorted.push(compiled.files["file://" + dropOrder[f].replace("../Drops", "").replace("../", "/") + ".js"])
+async function genHTML(): Promise<string> {
+	const dir = await Deno.readDir(import.meta.url.slice(7).split("/").slice(0, -2).join("/") + "/Drops")
+	const files = []
+	for await (const f of dir) {
+		if (!(f.name.includes(".t.")))
+			files.push(`<script src="../Drops/${f.name}"></script>`) // Doesn't account for different directories.
+	}
+	return `<body>
+		<canvas id="cnv"></canvas>
+		${files.join("")}
+		<script src="main.ts"></script>
+	</body>`
 }
 
-let built = sorted
-	.map(e => e.replace("\"use strict\";\n", ""))
-	.join("\n")
-
-if (FULL)
-	built = minify(Language.JS, built)
-
-Deno.writeTextFileSync("out.js", built)
+export { buildTS, genHTML }
