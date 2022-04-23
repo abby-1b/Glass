d380c1bbaba857de9b641a2cdb57359d4a1e4caa
CodeIGuess <codeiguess@gmail.com>
2022-04-23 11:33:02 -0400

    Removed when moving to Glass-r6

--- Drops/EventHolder.ts
+++
@@ -1,5 +0,0 @@
-class EventHolder {
-	// constructor() {
-
-	// }
-}
--- Drops/GObj.ts
+++
@@ -1,9 +0,0 @@
-
-class GObj extends TextureCanvas {
-	public parent: Scene
-
-	public pos: Vec2 = new Vec2(0, 0)
-	public _layer = 0
-
-	public draw(): void { }
-}
