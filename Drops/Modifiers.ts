05b7a025aeb5d4dc3889aaea2e52c777d6db5275
CodeIGuess <codeiguess@gmail.com>
2022-04-30 11:21:57 -0400

    Some work towards animations
    These will eventually work similarly to Godot's built-in animation system, just built into every sprite.

--- Drops/Modifiers.ts
+++ Drops/Modifiers.ts
@@ -1,12 +1,43 @@
 
+type ModifierArguments = (string | number)[]
+
+/**
+ * Applies modifier to objects.
+ */
+class Modifiable {
+	[key: string]: any
+
+	mods: {[key: string]: Modifier} = {}
+
+	/** Applies a specific modifier to this object. */
+	applyModifier(m: typeof Modifier): this {
+		const mod = new m(this)
+		this.mods[(m.name.match(/(?<=[a-z])[A-Z].*/) ?? "")[0]] = mod
+		return this
+	}
+}
+
 /**
  * Modifiers are my solution to a class extending multiple
  * classes. A modifier can manipulate a class however it
  * wants (within type constraints).
  */
-class Modifiable {
-	/** Applies a specific modifier to this object. */
-	applyModifier(): this {
-		return this
+class Modifier {
+	[key: string]: any
+	
+	parent: Modifiable
+	constructor(parent: Modifiable) { this.parent = parent }
+}
+
+/**
+ * Animates a property.
+ */
+class ModAnimation extends Modifier {
+	animSize = 0
+	properties: string[] = []
+	// propertyTriggers: {[key: string]: [number, number]} = []
+
+	addValue(v: number): void {
+		this.parent[this.property] += v
 	}
 }
