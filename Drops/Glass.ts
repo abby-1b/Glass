db2ffa1b436e33be43c20371768398d0b93f7690
CodeIGuess <codeiguess@gmail.com>
2022-05-04 20:47:04 -0400

    Comments (and a single variable name change)

--- Drops/Glass.ts
+++ Drops/Glass.ts
@@ -2,10 +2,17 @@
 let frameCount = 0
 let width: number, height: number
 
+/**
+ * Sets the desired size for the canvas. Only affects pixel mode.
+ * 
+ * **NOTE: THIS NEEDS TO BE CALLED FOR THE CANVAS TO BE INITIALIZED PROPERLY.**
+ * @param desiredSize 200 by default. Makes the canvas occupy a total area of this value squared, no matter the dimensions.
+ */
 function size(desiredSize = 200): void {
 	cSurface.pixelSize(desiredSize)
 	console.log("Set size to", desiredSize)
 
+	// TODO: pixel-size calculations.
 	width = 1
 	height = 1
 
@@ -15,7 +22,8 @@ function size(desiredSize = 200): void {
 }
 
 /**
- * Makes the canvas switch over to its real size, without scaling pixels. This should be used for full-resolution games or games that don't stick to a pixel-art style.
+ * Makes the canvas switch over to its real size, without scaling pixels.
+ * This should be used for full-resolution games or games that don't stick to a pixel-art style.
  */
 function noPixels(): void {
 	cSurface.realSize()
@@ -25,13 +33,18 @@ function preLoad(fn: () => void): void {
 	fn()
 }
 
+/**
+ * Sets the frame function, which runs every frame. It then starts the frame loop.
+ * 
+ * **NOTE: THIS NEEDS TO BE CALLED FOR THE PROGRAM TO START.**
+ */
 function frame(fn: () => void): void {
-	const frameFunction = (): void => {
+	const frameFn = (): void => {
 		cSurface.frame()
-		for (const t of cSurface.drawPool) t.draw()
+		root.frameFn()
 		fn()
 		frameCount++
-		window.requestAnimationFrame(frameFunction)
+		window.requestAnimationFrame(frameFn)
 	}
-	frameFunction()
+	frameFn()
 }
