3414c4b14d81902d0870c5da2781327f6913b23e
CodeIGuess <codeiguess@gmail.com>
2022-05-22 23:22:57 -0400

    Lightly commented
    Hoping to actually get some work done here now, since the AppInventor script works.

--- TestProject/main.ts
+++ TestProject/main.ts
@@ -3,16 +3,22 @@ export {}
 size(200)
 noPixels()
 
+// Create player character
 const player = new Sprite("Player")
 	.fromURL("./Assets/test.png")
 	.setPos(20, 16)
 	.setSize(256, 256)
 	.setRect(0, 0, 16)
 
-preLoad(() => {})
+// Preload (nothing lmao)
+preLoad(() => {
 
+})
+
+// Frame function, runs every frame.
 frame(() => {
-	// player.setRect(0, 0, mouseX, mouseY)
+	// Testing recrangles
+	player.setRect(0, 0, mouseX, mouseY)
 	player.frame = mouseY / width
 	colorf(map(mouseX, 0, width, 0, 255), map(mouseY, 0, height, 0, 255), 0)
 	rect(mouseX - 32, mouseY - 32, 64, 64)
