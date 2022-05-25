99452f4afe788504c9d65d0618bd35dbe16b5a62
CodeIGuess <codeiguess@gmail.com>
2022-05-24 21:48:28 -0400

    Update main.ts

--- TestProject/main.ts
+++ TestProject/main.ts
@@ -18,8 +18,9 @@ preLoad(() => {
 // Frame function, runs every frame.
 frame(() => {
 	// Testing recrangles
-	player.setRect(0, 0, mouseX, mouseY)
-	player.frame = mouseY / width
+	// player.setRect(0, 0, mouseX, mouseY)
+	// player.frame = mouseY / width
 	colorf(map(mouseX, 0, width, 0, 255), map(mouseY, 0, height, 0, 255), 0)
 	rect(mouseX - 32, mouseY - 32, 64, 64)
 })
+s
