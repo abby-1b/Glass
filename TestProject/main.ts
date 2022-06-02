8ed0dd4f7132b0160cc5d9694c2609bf21482446
CodeIGuess <codeiguess@gmail.com>
2022-06-01 22:04:07 -0400

    Update main.ts

--- TestProject/main.ts
+++ TestProject/main.ts
@@ -1,13 +1,18 @@
+/// BOILERPLATE ///
 export {}
 
-size(200)
-noPixels()
+pixelated(200)
+// realSize()
+
+cScene = new Scene("Scene1")
+root.addChild(cScene)
+/// NOT BOILERPLATE ///
 
 // Create player character
 const player = new Sprite("Player")
 	.fromURL("./Assets/test.png")
-	.setPos(20, 16)
-	.setSize(256, 256)
+	.setPos(10, 10)
+	.setSize(16, 16)
 	.setRect(0, 0, 16)
 
 // Preload (nothing lmao)
@@ -20,7 +25,6 @@ frame(() => {
 	// Testing recrangles
 	// player.setRect(0, 0, mouseX, mouseY)
 	// player.frame = mouseY / width
-	colorf(map(mouseX, 0, width, 0, 255), map(mouseY, 0, height, 0, 255), 0)
-	rect(mouseX - 32, mouseY - 32, 64, 64)
+	// colorf(map(mouseX, 0, width, 0, 255), map(mouseY, 0, height, 0, 255), 0)
+	// rect(mouseX - 32, mouseY - 32, 64, 64)
 })
-s
