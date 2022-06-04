830f49bc0dc575ad04351bd1527322ed74c1a1f6
CodeIGuess <codeiguess@gmail.com>
2022-06-03 23:19:51 -0400

    Update Physics.ts

--- Drops/Physics.ts
+++ Drops/Physics.ts
@@ -1,13 +1,16 @@
-/** */
-class PhysicsObject {
-	constructor() {
+/**
+ * A static object that interacts with other physics objects.
+ * Is not affected by gravity or other forces.
+ */
+class PhysicsObject extends Sprite {}
 
-	}
-}
+/**
+ * A movable object that interacts with other physics objects.
+ * Is affected by gravity and other forces.
+*/
+class PhysicsBody extends PhysicsObject {
+	velocity = new Vec2(0, 0)
 
-/** */
-class PhysicsBody {
-	constructor() {
-		
-	}
+	// TODO: implement physics in a loop
+	// Also try to figure out how to use delta for physics calculations... Please.
 }
