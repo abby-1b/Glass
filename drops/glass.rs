6d06df6dc3ae29a42f53404154c40757cfe163ce
CodeIGuess <codeiguess@gmail.com>
2022-06-20 23:01:52 -0400

    Made Glass a global

--- drops/glass.rs
+++ drops/glass.rs
@@ -1,27 +1,63 @@
-use gl::*;
-use scene::*;
+use std::sync::Mutex;
+use crate::scene::*;
+use crate::log::*;
 
-pub struct Glass {
-	gl: GL,
+#[cfg(to = "bin")]
+use crate::gl::binary::Graphics;
+#[cfg(not(to = "bin"))]
+use crate::gl::web::Graphics;
+
+pub struct GlassStruct {
+	pub gl: Graphics,
 	scenes: Vec<Scene>,
-	frame_fn: Option<fn(i32, Glass) -> ()>,
-	physics_fn: Option<fn(i32, Glass) -> ()>,
+	frame_fn: Option<fn(i32, &mut GlassStruct) -> ()>,
+	physics_fn: Option<fn(i32, &mut GlassStruct) -> ()>,
+
+	pub frame_count: u64,
 }
-impl Glass {
-	pub fn frame(&mut self, f: fn(i32, Glass) -> ()) {
+
+impl GlassStruct {
+	pub fn new() -> Result<GlassStruct, ()> {
+		if let Ok(gl) = Graphics::new() {
+			Ok(GlassStruct {
+				gl: gl,
+				scenes: Vec::new(),
+				frame_fn: None,
+				physics_fn: None,
+
+				frame_count: 0,
+			})
+		} else {
+			log!("Error initializing GL.");
+			Err(())
+		}
+	}
+
+	pub fn set_frame_fn(&mut self, f: fn(i32, &mut GlassStruct) -> ()) {
 		self.frame_fn = Some(f);
 	}
 
-	pub fn physics(&mut self, f: fn(i32, Glass) -> ()) {
+	pub fn set_physics_fn(&mut self, f: fn(i32, &mut GlassStruct) -> ()) {
 		self.physics_fn = Some(f);
 	}
-}
 
-pub fn new() -> Glass {
-	Glass {
-		gl: GL { },
-		scenes: Vec::new(),
-		frame_fn: None,
-		physics_fn: None,
+	pub fn init(&mut self) {
+		self.gl.init();
 	}
+
+	pub fn frame(&mut self) {
+		self.frame_count += 1;
+		self.gl.init_frame();
+		if self.frame_fn.is_some() { self.frame_fn.unwrap()(16, self); }
+	}
+
+	pub fn physics(&mut self) {
+		if self.physics_fn.is_some() { self.physics_fn.unwrap()(16, self); }
+	}
+}
+unsafe impl Send for GlassStruct {}
+
+// static Glass: SyncLazy<Mutex<Vec<u8>>> = SyncLazy::new(|| Mutex::new(vec![]));
+lazy_static! {
+	pub static ref Glass: Mutex<GlassStruct> = Mutex::new(GlassStruct::new().unwrap());
 }
