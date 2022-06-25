28b74dd47dd79530a34b1b7c86b7b4743f09972f
CodeIGuess <codeiguess@gmail.com>
2022-06-24 23:28:45 -0400

    Testing things

--- Projects/TestProject/main.ts
+++ Projects/TestProject/main.ts
@@ -1,12 +1,21 @@
+import { Glass } from "../../lib/Glass"
+import { Sprite } from "../../lib/Sprite"
 
-import "../../lib/Glass.ts"
+function setup() {
+	console.log(Glass)
+	Glass.scene.has(
+		new Sprite("Assets/test.png")
+	)
+}
 
-function frame() {
+function frame(delta: number) {
+	
+	Glass.colorf(255, 0, 0)
+	Glass.rect(10, 10, 50, 50)
+}
+
+function physics(delta: number) {
 
 }
 
-function physics() {
-
-}
-
-
+Glass.init(setup, frame, physics)
