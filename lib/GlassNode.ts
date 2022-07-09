d9b444a50b5fa762e74e88159a86f1454e14c9b0
CodeIGuess <codeiguess@gmail.com>
2022-07-09 01:20:52 -0400

    Added parent tracking and render/physics separation

--- lib/GlassNode.ts
+++ lib/GlassNode.ts
@@ -15,6 +15,7 @@ export class GlassNode {
 	size: Vec2 = new Vec2(0, 0)
 	showHitbox: boolean = true
 	children: GlassNode[] = []
+	parent: GlassNode | undefined
 
 	constructor() {
 		this.id = GlassNode.id++
@@ -38,20 +39,29 @@ export class GlassNode {
 	}
 
 	public has(...nodes: GlassNode[]): this {
+		nodes.forEach(n => n.parent = this)
 		this.children.push(...nodes)
 		return this
 	}
 
 	public render(delta: number) {
-		Glass.translate(this.pos.x, this.pos.y)
+		const x = Glass.isPixelated ? Math.floor(this.pos.x) : this.pos.x
+			, y = Glass.isPixelated ? Math.floor(this.pos.y) : this.pos.y
+		Glass.translate(x, y)
 		for (let c = 0; c < this.children.length; c++)
 			this.children[c].render(delta)
+		
 		// Draw hitbox
 		if (this.showHitbox) {
 			Glass.colorf(255, 0, 0)
 			Glass.rect(0, 0, this.size.x, this.size.y)
 		}
 
-		Glass.translate(-this.pos.x, -this.pos.y)
+		Glass.translate(-x, -y)
+	}
+
+	public physics(delta: number) {
+		for (let c = 0; c < this.children.length; c++)
+			this.children[c].physics(delta)
 	}
 }
