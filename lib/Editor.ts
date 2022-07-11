522ff65d2025363cffa5187ef6eac8a710d6b2fe
CodeIGuess <codeiguess@gmail.com>
2022-07-10 23:37:44 -0400

    Made the editor actually be useful(ish)

--- lib/Editor.ts
+++ lib/Editor.ts
@@ -1,11 +1,50 @@
 import { Glass, globalize } from "./Glass"
+import { GlassNode } from "./GlassNode"
 
 class EditorInstance {
+	text = ""
+
+	openPaths = ["Root"]
+	selected = ""
+
+	init() {
+		Glass.onInput(["mouseDown"], "editorClick", () => {
+			
+		})
+	}
+
+	protected renderNode(n: GlassNode, indent = 0, vertical = 0, level = 0, path = ""): number {
+		if (Glass.frameCount < 1) console.log(path)
+		const open = this.openPaths.includes(path)
+		const txt = (open ? "" : ">") + n.getName(true)
+
+		if (this.selected == path) {
+			Glass.colorf(0, 0, 0, 80)
+			Glass.fillRect(indent * 10 - 1, vertical * 10 - 1, txt.length * 10, 10)
+		}
+		Glass.colorf(0, 0, 0)
+		Glass.text(txt, indent * 10, vertical * 10)
+		if (!open) return 0
+		let added = 0
+		n.children.forEach(c => {
+			added++
+			added += this.renderNode(c, indent + 1, vertical + added, level + 1, path + "/" + c.getName(true))
+		})
+		return added
+	}
+
 	public render() {
+		Glass.translate(2, 2)
+		this.text += Glass.events.join("")
 		Glass.colorf(255, 255, 255)
-		Glass.fillRect(2, 2, 255, 255)
-		Glass.colorf(255, 0, 0)
-		Glass.text("According to all known laws of aviation, there is no way a bee should be able to fly.", 3, 3)
+		Glass.fillRect(0, 0, 255, 255)
+
+		Glass.colorf(0, 0, 0)
+		Glass.translate(2, 2)
+		this.renderNode(Glass.scene, 0, 0, 0, "Root")
+		Glass.translate(-2, -2)
+
+		Glass.translate(-3, -3)
 	}
 }
 export const Editor = new EditorInstance()
