e96feed82ca37b3783062530c5163c9e7bc4edd3
CodeIGuess <codeiguess@gmail.com>
2022-07-29 14:40:40 -0400

    Separated type things into a separate class

---
+++ Experimental/types.ts
@@ -0,0 +1,30 @@
+export type Type = string[]
+
+const opImportanceOrder = ["str", "f64", "f32", "i64", "i32"]
+export function operationReturns(operator: string, left: Type, right: Type): Type {
+	if (left.length > 0 || right.length > 0) console.log("Weird types here:", left, right)
+	const l = left[0]
+	const r = right[0]
+	if (l == r) return [l]
+	const li = opImportanceOrder.indexOf(l)
+	const ri = opImportanceOrder.indexOf(r)
+	if (li > ri) return [l]
+	else return [r]
+}
+
+export function typeMap(typeDict: {[key: string]: string}, type: string): string {
+	if (type in typeDict)
+		return typeDict[type]
+	else
+		return type
+}
+
+export function equalTypes(a: Type, b: Type): boolean {
+	if (a === b) return true
+	if (a == null || b == null) return false
+	if (a.length !== b.length) return false
+
+	for (let i = 0; i < a.length; ++i)
+		if (!b.includes(a[i])) return false
+	return true
+}
