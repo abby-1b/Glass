1ae3d5a868ca187efe10f97ad64bf2b0546adec4
CodeIGuess <codeiguess@gmail.com>
2022-07-29 14:40:48 -0400

    Testing

--- Experimental/convert.ts
+++ Experimental/convert.ts
@@ -1,9 +1,10 @@
 
-import { TreeNode, parse } from "./parse.ts"
+import { parse } from "./parse.ts"
 import * as js from "./langs/js.ts"
 
 const code = `
 fn main(arg0: i32, arg1: f32) { return 0 }
+fn main2(arg0: i32, arg1: f32): f32 { return 0 }
 `
 
 const nodes = parse(code)
