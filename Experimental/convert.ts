a6532db76ecb81b8d073f59bd38460e95375fe63
CodeIGuess <codeiguess@gmail.com>
2022-07-31 19:04:22 -0400

    Testing

--- Experimental/convert.ts
+++ Experimental/convert.ts
@@ -4,14 +4,15 @@ import { LangBase } from "./langs/langs.ts"
 // import * as js from "./langs/js.ts"
 // import * as js from "./langs/js.ts"
 
-const language = "java"
+const language = "js"
 const languageConverter = ((await import("./langs/" + language + ".ts")) as {Lang: typeof LangBase}).Lang
 
 const code = `
-fn main(a: f32, b: f32): f32 {
-	return a + b
+fn main(a: f32, b: f32): str {
+	return "str" == 0
 }
 `
+
 // fn main(arg0: i32, arg1: f32) { return 0 }
 // fn main2(arg0: i32, arg1: f32): f32 { return 0f }
 
