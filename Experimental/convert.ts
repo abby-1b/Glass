1be3c755de85644de4006ed026a9e6b8c9be7f63
CodeIGuess <codeiguess@gmail.com>
2022-07-30 21:38:55 -0400

    Added language-switching

--- Experimental/convert.ts
+++ Experimental/convert.ts
@@ -1,13 +1,21 @@
 
 import { parse } from "./parse.ts"
-import * as js from "./langs/js.ts"
+import { LangBase } from "./langs/langs.ts"
+// import * as js from "./langs/js.ts"
+// import * as js from "./langs/js.ts"
+
+const language = "java"
+const languageConverter = ((await import("./langs/" + language + ".ts")) as {Lang: typeof LangBase}).Lang
 
 const code = `
-fn main(arg0: i32, arg1: f32) { return 0 }
-fn main2(arg0: i32, arg1: f32): f32 { return 0 }
+fn main(a: f32, b: f32): f32 {
+	return a + b
+}
 `
+// fn main(arg0: i32, arg1: f32) { return 0 }
+// fn main2(arg0: i32, arg1: f32): f32 { return 0f }
 
 const nodes = parse(code)
-const out = js.Lang.takeArr(nodes)
+const out = languageConverter.takeArr(nodes)
 
 console.log(out)
