336e8047dc5dc762144ba7702bf4c30dec760ba0
CodeIGuess <codeiguess@gmail.com>
2022-08-04 20:12:38 -0400

    Made types into single values
    Helps with compatibility with literally any language other than TypeScript.

--- Experimental/types.ts
+++ Experimental/types.ts
@@ -3,11 +3,10 @@
 // str
 // str[]
 export class PrimitiveType {
-	protected isSpreadArg = false
 	protected _isArray = false
-	protected types: Set<string> = new Set<string>()
+	protected type = ""
 
-	constructor(...types: string[]) { types.forEach(t => this.types.add(t)) }
+	constructor(type: string = "") { this.type = type }
 
 	array(): this {
 		this._isArray = true
@@ -15,36 +14,38 @@ export class PrimitiveType {
 	}
 	isArray(): boolean { return this._isArray }
 
-	set(type: PrimitiveType): void { this.types = new Set(type.types) }
-	setStr(...str: string[]) { this.types = new Set(str) }
-	isSet(): boolean { return this.types.size != 0 }
+	set(type: PrimitiveType): void { this.type = type.type }
+	setStr(str: string) { this.type = str }
+	isSet(): boolean { return this.type != "" }
 
-	merge(type: PrimitiveType) { type.types.forEach(t => this.types.add(t)) }
+	// merge(type: PrimitiveType) { type.types.forEach(t => this.types.add(t)) }
 
 	getOperatorReturn(type: PrimitiveType): PrimitiveType {
-		if (this.types.size > 1) console.log("WEIRD TYPE A")
-		if (type.types.size > 1) console.log("WEIRD TYPE B")
-		const [l] = this.types
-		const [r] = type.types
-		if (l == r) return this
-		const li = opImportanceOrder.indexOf(l)
-		const ri = opImportanceOrder.indexOf(r)
-		if (li > ri) return this
+		if (this.type == type.type) return this
+		const li = opImportanceOrder.indexOf(this.type)
+		const ri = opImportanceOrder.indexOf(type.type)
+		if (li < ri) return this
 		else return type
 	}
 
 	equals(type: PrimitiveType): boolean {
-		if (this.types.size !== type.types.size) return false
-		return [...this.types].every((x) => type.types.has(x))
+		return this.type == type.type
+			&& this._isArray == type._isArray
 	}
 
-	getTypes(): string[] {
-		return [...this.types]
+	toString(format = false) {
+		return (format ? "\u001b[33m" : "") + this.type + (format ? "\u001b[0m" : "")
 	}
 }
 
 export class FunctionType extends PrimitiveType {
 	args: PrimitiveType[] = []
+
+	constructor(fn: {type: PrimitiveType, args: {type: PrimitiveType}[]}) {
+		super(fn.type.toString())
+		if (fn.type.isArray()) this.array()
+		this.args = fn.args.map(a => a.type)
+	}
 }
 
 const opImportanceOrder = ["str", "f64", "f32", "i64", "i32"]
