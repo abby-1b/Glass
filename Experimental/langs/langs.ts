49f42c7975d79396ad3d1d0d6ddb7a3375f14b81
CodeIGuess <codeiguess@gmail.com>
2022-08-04 20:14:35 -0400

    Unified basic language constructs
    Every basic language thing is in the base `Lang` class, so it can be overriden by different languages to suit different requirements.

--- Experimental/langs/langs.ts
+++ Experimental/langs/langs.ts
@@ -1,9 +1,71 @@
-import { TreeNode } from "../parse.ts"
-export class LangBase {
-	static takeArr(nodes: TreeNode[]): string { return nodes.map(n => this.take(n)).join("\n") }
-	static take(node: TreeNode): string { console.log("Language features not implemented!"); return "" }
+import { PrimitiveType, typeMap } from "../types.ts"
+import {
+	TreeNode,
+	FunctionNode,
+	StatementNode,
+	VarNode,
+	LetNode,
+	SetNode,
+	OperatorNode,
+	NumberLiteralNode,
+	StringLiteralNode,
+	ConditionNode,
+	CallNode,
+	ParenNode,
+} from "../parse.ts"
 
-	static indent(str: string) {
-		return str.split("\n").map(e => "\t" + e).join("\n")
+export class Lang {
+	static typeMappings = {}
+	static operations: {[key: string]: (a: TreeNode, b: TreeNode) => string} = {}
+
+	static lineEnding() { return "\n" }
+	static convertType(type: PrimitiveType) { return typeMap(this.typeMappings, type.toString()) }
+
+	static takeArr(nodes: TreeNode[]): string { return nodes.map(n => this.take(n) + this.lineEnding()).join("") }
+
+	static indent(str: string) { return str.split("\n").map(e => "\t" + e).join("\n") }
+
+	static take(node: TreeNode): string {
+		if (node instanceof FunctionNode) return this.takeFunctionNode(node)
+		if (node instanceof StatementNode) return this.takeStatementNode(node)
+		if (node instanceof OperatorNode) return this.takeOperatorNode(node)
+		if (node instanceof CallNode) return this.takeCallNode(node)
+		if (node instanceof SetNode) return this.takeSetNode(node)
+		if (node instanceof ParenNode) return this.takeParenNode(node)
+		if (node instanceof VarNode) return this.takeVarNode(node)
+		if (node instanceof StringLiteralNode) return this.takeStringLiteralNode(node)
+		if (node instanceof NumberLiteralNode) return this.takeNumberLiteralNode(node)
+		if (node instanceof ConditionNode) return this.takeConditionNode(node)
+		if (node instanceof LetNode) return this.takeLetNode(node)
+
+		console.log("Node not found:", node)
+		return "..."
 	}
+	
+	static takeLetNode(node: LetNode) { return "let " + node.name + ": " + this.convertType(node.type) + " = " + this.take(node.value) }
+	static takeOperatorNode(node: OperatorNode) {
+		const str = node.left!.type.toString() + node.name + node.right!.type.toString()
+		if (str in this.operations)
+			return "("
+				+ this.operations[str](node.left!, node.right!) + ")"
+		return "(" + this.take(node.left!) + " " + node.name + " " + this.take(node.right!) + ")"
+	}
+	static takeConditionNode(node: ConditionNode) {
+		return node.name + " (" + this.take(node.condition) + ") {\n"
+			+ this.indent(this.takeArr(node.children))
+			+ "\n}"
+	}
+	static takeCallNode(node: CallNode) { return node.callVar.name + this.take(node.callParen) }
+	static takeParenNode(node: ParenNode) { return "(" + this.takeArr(node.children) + ")" }
+	static takeNumberLiteralNode(node: NumberLiteralNode) { return node.value }
+	static takeStringLiteralNode(node: StringLiteralNode) { return node.value }
+	static takeSetNode(node: SetNode) { return this.take(node.setting) + " = " + this.take(node.value) }
+	static takeFunctionNode(node: FunctionNode) {
+		return "fn " + node.name
+			+ "(" + node.args.map(v => v.name + ": " + this.convertType(v.type)).join(", ") + "): " + this.convertType(node.type) + " {\n"
+			+ this.indent(this.takeArr(node.children))
+			+ "\n}"
+	}
+	static takeStatementNode(node: StatementNode) { return node.name + " " + this.take(node.arg) }
+	static takeVarNode(node: VarNode) { return node.name }
 }
