b90ff1af1ac74c5e2b337d1800635b9ca9afad91
CodeIGuess <codeiguess@gmail.com>
2022-08-07 00:12:25 -0400

    Testing function returns!

--- Experimental/convert.ts
+++ Experimental/convert.ts
@@ -4,24 +4,27 @@ import { Lang } from "./langs/langs.ts"
 // import * as js from "./langs/js.ts"
 // import * as js from "./langs/js.ts"
 
-const language = "js"
+const language = "ts"
 const languageConverter = ((await import("./langs/" + language + ".ts")) as {Lang: typeof Lang}).Lang
 
 const code = `
 fn main(a: i32, b: i32) {
-	let aa = a
-	let bb = b
-	while aa > 0 {
-		bb = bb + aa
-		aa = aa - 1
-	}
-	return "" + bb
+	return a + b
+	// let aa = a
+	// let bb = b
+	// while aa > 0 {
+	// 	bb = bb + aa
+	// 	aa = aa - 1
+	// }
+	// return "" + bb
 }
-let a: i32 = main()
+
+let a = main(10, 20)
+console.log(a.prop)
 `
 
 const nodes = parse(code)
 const out = languageConverter.takeArr(nodes)
 
 console.log(out)
-eval(out)
+// eval(out)
