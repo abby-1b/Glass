ec2eca6114abd5fb557750c1850e8895260a625b
CodeIGuess <codeiguess@gmail.com>
2022-08-07 17:43:29 -0400

    Implemented fileId for tokens

--- Experimental/tokens.ts
+++ Experimental/tokens.ts
@@ -2,6 +2,7 @@
 export interface TokenRange {
 	start: number
 	end: number
+	fileId?: number
 }
 export interface Token extends TokenRange {
 	val: string
@@ -12,6 +13,16 @@ export function expandRange(range: TokenRange, ...tokens: Token[]): Token[] {
 		if (tokens[t].start < range.start) range.start = tokens[t].start
 		if (tokens[t].end > range.end) range.end = tokens[t].end
 	}
+	range.fileId = tokens[0].fileId
 	return tokens
+}
+
+export function expandRangeNodes(range: TokenRange, ...nodes: {range: TokenRange}[]) {
+	for (let t = 0; t < nodes.length; t++) {
+		if (nodes[t].range.start < range.start) range.start = nodes[t].range.start
+		if (nodes[t].range.end > range.end) range.end = nodes[t].range.end
+	}
+	range.fileId = nodes[0].range.fileId
+	return nodes
 }
 
