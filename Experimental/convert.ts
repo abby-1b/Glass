833fa96459fc7a772bc74b88d2a6153285ecb847
CodeIGuess <codeiguess@gmail.com>
2022-08-08 12:46:24 -0400

    Moved to a separate file

--- Experimental/convert.ts
+++ Experimental/convert.ts
@@ -4,27 +4,13 @@ import { Lang } from "./langs/langs.ts"
 // import * as js from "./langs/js.ts"
 // import * as js from "./langs/js.ts"
 
-const language = "ts"
+const language = "js"
 const languageConverter = ((await import("./langs/" + language + ".ts")) as {Lang: typeof Lang}).Lang
 
-const code = `
-fn main(a: i32, b: i32): str {
-	return a + b
-	// let aa = a
-	// let bb = b
-	// while aa > 0 {
-	// 	bb = bb + aa
-	// 	aa = aa - 1
-	// }
-	// return "" + bb
-}
-
-let a: str[] = main(10, 20)
-// console.log(a.prop)
-`
+const code = Deno.readTextFileSync("test.sl")
 
 const nodes = parse(code)
 const out = languageConverter.takeArr(nodes)
 
-console.log(out)
-// eval(out)
+// console.log(out)
+eval(out)
--- Experimental/test.sl
+++ Experimental/test.sl
@@ -1,3 +1,8 @@
-# Sl stands for "simple language"
+// Sl stands for "simple language"
 
+fn arr(a: i32, b: i32) {
+	return [a, b]
+}
 
+let ok = arr(10, 20)
+console.log(ok)
