93892eabab76fb01d2f7c6a4f4c31f322ef0960d
CodeIGuess <codeiguess@gmail.com>
2022-10-17 22:03:07 -0400

    Implemented unloading

--- src/Nodes/Scene.ts
+++ src/Nodes/Scene.ts
@@ -4,9 +4,10 @@
  * Controls the loading and unloading of a scene.
  */
  class Scene extends GlassNode {
-	/** The currently active camera. */
-	static current?: Camera
-
 	pos: Vec2 = new Vec2(0, 0)
-	centered = true
+	// centered = true
+
+	unload() {
+		this.children = []
+	}
 }
