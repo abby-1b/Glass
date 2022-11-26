dcce7765b40454f4cd0e6b6e087ca2892fd179c0
CodeIGuess <codeiguess@gmail.com>
2022-11-25 23:17:58 -0400

    Removed, this was basically used for testing

--- editor/spriteScript.ts
+++
@@ -1,7 +0,0 @@
-
-export function loop(this: Sprite) {
-	if (Input.keys.includes("d"))
-		((this.children[0] as AnimationNode).play("run"))
-	else 
-		((this.children[0] as AnimationNode).play("ball"))
-}
