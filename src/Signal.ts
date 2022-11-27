502d10b890e45cceaa1ca3a87efb74578c09bb34
CodeIGuess <codeiguess@gmail.com>
2022-11-26 23:28:12 -0400

    Made signals be able to be activated by any object

--- src/Signal.ts
+++ src/Signal.ts
@@ -5,15 +5,15 @@
  * Note: yes, strings are still objects in JavaScript. Let me have my microoptimization.
 */
 class Signal {
-	static list: {[key: string]: ((n: GlassNode) => void)[]} = {}
+	static list: {[key: string]: ((n: any) => void)[]} = {}
 
 	/** Adds a listener for a signal. When this signal is triggered, all of the events tied to it will be ran in order. */
-	static addListener(signalName: string, fn: (n: GlassNode) => void) {
+	static addListener(signalName: string, fn: (n: any) => void) {
 		this.list[signalName] ? this.list[signalName].push(fn) : this.list[signalName] = [fn]
 	}
 
 	/** Triggers a signal by name. */
-	static trigger(signalName: string | undefined, n: GlassNode) {
+	static trigger(signalName: string | undefined, n: any) {
 		if (!(signalName! in this.list)) return
 		for (let i = 0; i < this.list[signalName!].length; i++)
 			this.list[signalName!][i](n)
