ab17e1bdf243f7ff9f72277342d01bb04af7493e
CodeIGuess <codeiguess@gmail.com>
2022-11-28 22:11:20 -0400

    `writeText` method + Lightly commented

--- libTargets/webLocal/src/Local.ts
+++ libTargets/webLocal/src/Local.ts
@@ -1,6 +1,22 @@
 
 class Local {
-	getText() {
-		
+	private static projectOffset = ""
+	
+	static async getText(path: string): Promise<string> {
+		return fetch(this.projectOffset + path).then(r => {
+			if (r.ok == false) throw r
+			return r.text()
+		})
+	}
+
+	static async writeText(path: string, txt: string, reason?: string): Promise<void> {
+		return new Promise((resolve, reject) =>
+			fetch(this.projectOffset + path, {
+				method: "POST",
+				body: JSON.stringify(reason ? { txt, reason } : { txt })
+			})
+				.then(_ => resolve())
+				.catch(_ => reject())
+		)
 	}
 }
--- src/Local.d.ts
+++ src/Local.d.ts
@@ -1,7 +1,17 @@
-
 /**
- * Provides an interface with the local saving/loading server.
+ * Provides an interface with the local saving/loading implementation.
  */
 declare class Local {
+	/**
+	 * Gets a text file from the local file system.
+	 * @param path The file to fetch the text from
+	 */
 	public static getText(path: string): Promise<string>;
+	/**
+	 * Writes a text file to the local file system.
+	 * @param path The file to write text into
+	 * @param txt The text to write into the file
+	 * @param reason A reason why the text file was written (optional)
+	 */
+	static writeText(path: string, txt: string, reason?: string): Promise<void>
 }
