f4fff05f3f84c3c305151db54e0c8f758505a4af
CodeIGuess <codeiguess@gmail.com>
2022-11-28 22:10:56 -0400

    Shorter serialization + Lightly commented

--- src/Serialization.ts
+++ src/Serialization.ts
@@ -1,4 +1,3 @@
-/// <reference path="Loader.ts" />
 
 /** Serializes objects into a string. */
 class Serializer {
@@ -7,8 +6,21 @@ class Serializer {
 
 	/** Serializes an object into JSON format. */
 	static serialize(obj: any) {
+		// Serialize the object and remove its outer syntax (it's inferrable)
 		let ret = this.serializeObject(obj).slice(3, -1)
-		ret = this.references.map(r => this.serializeKey(r.constructor.name)).join(",") + "\n" + ret
+
+		// Serialize its reference names
+		let refStr: string[] = []
+		for (let i = 0; i < this.references.length; i++) {
+			let k = this.serializeKey(this.references[i].constructor.name)
+			const idx = refStr.indexOf(k)
+			if (idx == -1) 
+				refStr.push(k)
+			else
+				refStr.push("@" + idx.toString(36))
+		}
+
+		ret = refStr.join(",") + "\n" + ret
 		this.defaults = {}, this.references = []
 		return ret
 	}
@@ -21,7 +33,7 @@ class Serializer {
 		const objType: string = obj.constructor.name
 		if (objType == "String") return '"' + obj.replace(/"/g, "\\\"") + '"'
 		else if (objType == "Boolean") return obj ? "t" : "f"
-		else if (objType == "Number") return obj % 1 == 0 ? obj : ("#" + this.floatToString(obj))
+		else if (objType == "Number") return this.floatToString(obj)
 		else if (objType == "Array") isArray = true
 
 		// If already in reference, return that.
@@ -35,6 +47,7 @@ class Serializer {
 			.filter(e => typeof e[1].get === 'function' && e[0] !== '__proto__')
 			.map(e => e[0]))
 
+		// Put all of its properties into a single string, recursively serializing objects inside of it
 		let ret = "@" + this.references.length.toString(36) + (isArray ? "[" : "{")
 		this.references.push(obj)
 		let m = false
@@ -46,8 +59,9 @@ class Serializer {
 				m = true
 			}
 		}
-		// console.log(keys)
 
+		// If its modified, remove the last comma and close with the proper syntax.
+		// Otherwise, remove the original opening syntax ("{" or "[").
 		if (m) return ret.slice(0, -1) + (isArray ? "]" : "}")
 		else return ret.slice(0, -1)
 	}
@@ -62,7 +76,14 @@ class Serializer {
 			, f = new Float64Array(buf)
 			, i = new BigUint64Array(buf)
 		f[0] = n
-		return i[0].toString(36)
+		const ret = i[0].toString(36)
+
+		// If the string representation is simple (a.k.a doesn't use the full extent of float), just save it as human-readable
+		// Otherwise send this representation of float.
+		if ((n + "").length * 1.2 < ret.length)
+			return n + ""
+		else
+			return "#" + ret
 	}
 
 	private static instanceOfConstructor(cns: typeof Object) {
@@ -75,17 +96,39 @@ class DeSerializer {
 	private static refs: any[] = []
 	private static data: string
 
+	/** De-serializes a string into an object (with support for circular dependencies) */
 	static deSerialize(d: string) {
+		// Parse the object names into an array of references to these objects.
 		this.data = "," + d
-		while (this.data[0] != "\n") this.data = this.data.slice(1), this.refs.push(this.makeInstance(this.getKey()))
+		const names: string[] = []
+		while (this.data[0] != "\n") {
+			this.data = this.data.slice(1)
+			let k = this.getKey()
+			if (k[0] == "@") k = names[parseInt(k.slice(1))]
+			names.push(k)
+			this.refs.push(this.makeInstance(k))
+		}
+
+		// Check if the main object is an array.
 		let isArr = Array.isArray(this.refs[0])
+
+		// If it is, pad it with the array syntax. Otherwise, use object syntax.
 		this.data = (isArr ? "@0[" : "@0{") + this.data.slice(1) + (isArr ? "]" : "}")
+
+		// Then, parse the string into an object.
 		this.parseObject()
-		return this.refs[0]
+		
+		// Save the returned object
+		const ret = this.refs[0]
+
+		// Empty out the references
+		while (this.refs.length > 0) this.refs.pop()
+
+		return ret
 	}
 
 	private static parseObject() {
-		let obj = this.refs[parseInt(this.getKey().slice(1), 36)]
+		let obj = this.refs![parseInt(this.getKey().slice(1), 36)]
 			, close = this.data[0] == "{" ? "}" : "]"
 			, isArray = close == "]"
 			, arrayIdx = 0
@@ -106,6 +149,7 @@ class DeSerializer {
 				obj[k] = s
 			} else if (this.data[0] == "t" || this.data[0] == "f") obj[k] = this.data[0] == "t", this.data = this.data.slice(1)
 			else if (this.data[0] == "w") obj[k] = this.makeInstance("WebGLTexture"), this.data = this.data.slice(1)
+			else if (this.data[0] == "#") obj[k] = this.stringToFloat(this.data.slice(1, 13)), this.data = this.data.slice(13)
 			else obj[k] = parseFloat(this.getKey())
 			// TODO: implement serialized float to float conversion
 
