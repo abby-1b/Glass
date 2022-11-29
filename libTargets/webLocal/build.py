45e4e1e6eca6180e6bf6683b5bd15eddb951320d
CodeIGuess <codeiguess@gmail.com>
2022-11-28 22:12:39 -0400

    Implemented project path offset for webLocal testing
    also error handling noice

--- libTargets/webLocal/build.py
+++ libTargets/webLocal/build.py
@@ -1,15 +1,17 @@
-from os import system, remove, rename #, listdir
+from os import system, remove #, rename
 
 def compile(path: str):
+	# Stores if everything went nicely.
+	good = True
+
 	# Make temporary ts config to include file 
 	with open("./tempConfig.json", "w") as f:
 		f.write('{"extends":"./tsconfig.json","include":["../../libOutputs/util.ts","src/**/*.ts","../../libOutputs/lib.ts","' + path + '/**/*.ts"]}')
 
-	system("tsc --project tempConfig.json") # Run `tsc` with the temporary ts config
+	good = system("tsc --project tempConfig.json") == 0 # Run `tsc` with the temporary ts config
 	remove("./tempConfig.json") # Remove the temporary config file
 
-	# exit()
-	rename("./lib.js", "../../libOutputs/webLocal/lib.js")
-
 	with open("../../libOutputs/webLocal/index.html", "w") as f:
-		f.write('<script src="./lib.js"></script><script>Loader.init()</script>')
+		f.write('<script src="./lib.js"></script><script>Local.projectOffset="' + path + '/"\nLoader.init()</script>')
+
+	return good
