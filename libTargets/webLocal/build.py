8bdf027585288214326e1746e2000bd02bd36369
CodeIGuess <codeiguess@gmail.com>
2022-11-29 21:38:50 -0400

    Implemented watch script!

--- libTargets/webLocal/build.py
+++ libTargets/webLocal/build.py
@@ -1,6 +1,7 @@
-from os import system, remove #, rename
-
+# Compile a project path
 def compile(path: str):
+	from os import system, remove
+
 	# Stores if everything went nicely.
 	good = True
 
@@ -15,3 +16,31 @@ def compile(path: str):
 		f.write('<script src="./lib.js"></script><script>Local.projectOffset="' + path + '/"\nLoader.init()</script>')
 
 	return good
+
+# Watch for file changes at a path
+def watch(path: str):
+	from time import sleep, time
+	from os import listdir
+	from os.path import getmtime
+
+	print("Watching for changes to", path)
+	while True:
+		sleep(2)
+		listdir(path)
+
+		t = time()
+		for f in listdir(path):
+			m = getmtime(path + "/" + f)
+
+			# Check if it was saved less than two seconds ago
+			if t - m < 2:
+				compile(path)
+				break
+
+# If it's not being ran as a module, start the watch process
+if __name__ == "__main__":
+	from sys import argv
+	if len(argv) < 2:
+		print("No project path provided!")
+		exit()
+	watch(argv[1])
