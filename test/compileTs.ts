9fc4cc4d47c1591ea07ff86db719edaad353c1dc
CodeIGuess <codeiguess@gmail.com>
2022-12-06 21:24:33 -0400

    Testing TS compilation from Deno

---
+++ test/compileTs.ts
@@ -0,0 +1,15 @@
+import { emit } from "https://deno.land/x/emit@0.0.1/mod.ts"
+
+let url = "data:text/typescript;base64," + btoa("console.log(10)")
+
+console.log(url)
+
+let code = (await emit(url))[url]
+
+let i = code.length - 5; while (code[i] != ",") i--; i++
+
+const sMap = JSON.parse(atob(code.slice(i)))
+sMap.sources[0] = "./someSource.ts"
+code = code.slice(0, i) + btoa(JSON.stringify(sMap))
+
+console.log(code)
