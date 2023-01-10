86ade7831d7f641c0538a63a8796510d1626c79d
CodeIGuess <codeiguess@gmail.com>
2023-01-09 21:52:32 -0400

    Implemented some camera functionality
    Not functional yet, but it's just base code. Soon it'll work :)

--- src/Nodes/Camera.ts
+++ src/Nodes/Camera.ts
@@ -5,18 +5,46 @@
  */
 class Camera extends GlassNode {
 	protected static saveProperties = [
-		"centered"
+		"pos", "centered", "zoom"
 	]
 	/** The currently active camera. */
 	static current?: Camera
 
 	pos: Vec2 = new Vec2(0, 0)
+	zoom: Vec2 = new Vec2(1, 1)
+	rot: number = 0
 	centered = true
+
+	private currTransform = new Float32Array(9)
+
+	protected transform(forward: boolean) {
+		if (forward) {
+			GL.translate(this.pos.x, this.pos.y)
+			GL.rotate(this.rot)
+		} else {
+			GL.rotate(-this.rot)
+			GL.translate(-this.pos.x, -this.pos.y)
+		}
+	}
 
 	constructor(name?: string) {
 		super(name)
 
-		// If there is no active camera, just put this newly generated one in there.
+		// If there is no active camera, put this newly instanced one there
 		;(!Camera.current) && (Camera.current = this)
+	}
+
+	/** Uses this camera as the frame of reference for the current frame. This gets called by Glass automatically. */
+	private use() {
+		if (!this.parent) return
+		
+		let p = this as GlassNode
+		const parents = [p]
+		while (p != GlassRoot) p = p.parent!, parents.push(p)
+		
+		for (let i = 0; i < parents.length; i++) {
+		// for (let i = parents.length - 1; i >= 0; i--) {
+			(<any>parents[i]).transform(true)
+		}
 	}
 }
